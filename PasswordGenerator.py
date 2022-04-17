4735ff13841f9c2001065cab32a0e37bd04eab1a
raj <rajkokate299@gmail.com>
2022-04-17 15:04:36 -0400

    new file added finally

---
+++ PasswordGenerator.py
@@ -0,0 +1,10 @@
+import random
+import string
+lower="rajdattnarendrakokate"
+upper="RAJDATTNARENDRAKOKATE"
+numbers="1234567890"
+symbols="[]{}()$&*"
+all=lower+upper+numbers+symbols
+length=16
+password="".join(random.sample(all,length))
+print(password)
